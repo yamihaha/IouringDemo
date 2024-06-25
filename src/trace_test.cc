b129cf4bbc8b8cb3bf26375548182be26503db6b
yamihaha <2579010536@qq.com>
2024-06-25 20:47:14 +0800

    support Direct I/O

--- src/trace_test.cc
+++ src/trace_test.cc
@@ -1,129 +1,114 @@
-#include <cstdio>
-#include <iostream>
-#include <liburing.h>
+
+#include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
-#include <cstring>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <cstdlib>
+#include "liburing.h"
 
-using namespace std;
+#define QD	1
+#define BUF_SIZE 4096 
 
-#define RING_QD (32)
-#define RING_FLAG (IORING_SETUP_IOPOLL)
-
-
-#define KB (1024LL)
-#define MB (1024 * 1024LL)
-#define GB (1024 * 1024 * 1024LL)
-
-#define MAX_IOSIZE (10 * MB)
-
-#define USER_SPACE_LEN (10 * GB)
-
-
-struct io_request {
-    int fd;
-    uint64_t offset;
-    uint64_t len;
-    char *buf;
-    bool read;
-};
-
-template <class Container>
-void str_split(const string& str, Container& cont,
-              const string& delims = " ")
+int main(int argc, char *argv[])
 {
-    size_t current, previous = 0;
-    current = str.find_first_of(delims);
-    while (current != string::npos) {
-        cont.push_back(str.substr(previous, current - previous));
-        previous = current + 1;
-        current = str.find_first_of(delims, previous);
-    }
-    cont.push_back(str.substr(previous, current - previous));
-}
+	struct io_uring ring;
+	int i, fd, ret, pending, done;
+	struct io_uring_sqe *sqe;
+	struct io_uring_cqe *cqe;
+	//struct iovec *iovecs;
+	struct stat sb;
+	ssize_t fsize;
+	off_t offset;
+	void *buf;
 
+	/*if (argc < 2) {
+		printf("%s: file\n", argv[0]);
+		return 1;
+	}*/
 
-void submit_io(struct io_uring *ring, struct io_request *req) {
-    //打印IO请求
-    cout <<" offset: " << req->offset << " len: " << req->len << " read: " << req->read << endl;
-    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-    io_uring_prep_rw(req->read ? IORING_OP_READ : IORING_OP_WRITE, sqe, req->fd, req->buf, req->len, req->offset);
-    // printf("sqe->user_data:%llu\n", sqe->user_data);
-    io_uring_submit(ring);
-}
+	ret = io_uring_queue_init(QD, &ring, 0);
+	if (ret < 0) {
+		fprintf(stderr, "queue_init: %s\n", strerror(-ret));
+		return 1;
+	}
 
-void wait_completion(struct io_uring *ring) {
-    struct io_uring_cqe *cqe;
-    io_uring_wait_cqe(ring, &cqe);
-    // printf("cqe->user_data:%llu\n", cqe->user_data);
-    //打印res
-    cout << "res: " << cqe->res << endl;
-    io_uring_cqe_seen(ring, cqe);
-}
+	//fd = open(argv[1], O_RDONLY | O_DIRECT); 
+	//fd = open("/home/femu/do.sh", O_RDWR | 040000); 
+	fd = open("/dev/nvme0n1", O_RDWR | O_DIRECT);
+	//printf("%d\n",fd);
+	if (fd < 0) {
+		perror("open");
+		return 1;
+	}
 
-int main() {
-    struct io_uring ring;
-    io_uring_queue_init(RING_QD, &ring, RING_FLAG);
+	if (fstat(fd, &sb) < 0) {
+		perror("fstat");
+		return 1;
+	}
 
-    // Open your file here
-    int fd = open("/dev/nvme0n1", O_RDWR | O_CREAT, 0644);
-    if (fd < 0) {
-        perror("open");
-        return 1;
-    }
+	fsize = 0;
+	struct iovec *iovecs =(struct iovec*) calloc(QD, sizeof(struct iovec));
 
-    // Allocate buffer for IO
-    char *buffer = new char[MAX_IOSIZE];
-    if (!buffer) {
-        std::cerr << "Failed to allocate buffer" << std::endl;
-        return 1;
-    }
-    string line;
-    ifstream  file("../Trace/mytest.log");
-    if (!file.is_open()) {
-        cerr << "Failed to open trace file." << endl;
-        return 1;
-    }
-    while (getline(file, line)) {
-        if (line.empty())
-            continue;
-        vector<string> lineSplit;
-        str_split(line, lineSplit, "\t");
-        uint64_t offset = atoll(lineSplit[1].c_str());
-        uint64_t length = atoll(lineSplit[2].c_str());
-        if (length > 10 * MB) {
-            continue;
-        }
-        offset = offset % USER_SPACE_LEN;
-        offset = offset / (4 * KB) * (4 * KB);
+        // buf = malloc(BUF_SIZE);
+		posix_memalign((void **)&buf, 512, BUF_SIZE);
+		
+		iovecs[i].iov_base = buf;
+		iovecs[i].iov_len = 4096;
+		fsize += 4096;
 
-        cout << "offset: " << offset << " length: " << length << endl;
+	offset = 0;
+    //i = 0;
+	//do {
+		sqe = io_uring_get_sqe(&ring);
+		//if (!sqe)
+		//	break;
+		io_uring_prep_readv(sqe, fd, &iovecs[0], 1, offset);
+		sqe->added_info=29;
+		offset += iovecs[0].iov_len;
+	//	i++;
+		//if (offset >= sb.st_size)
+		//	break;
+	//} while (1);
 
-        if (lineSplit[0] == "R") {
-            // Read operation
-            io_request req = {fd, offset, length, buffer, true};
-            submit_io(&ring, &req);
-            wait_completion(&ring);
-        } else if (lineSplit[0] == "W") {
-            // Write operation
-            //  Fill buffer with random data
-            for (int i = 0; i < length; i++) {
-                buffer[i] = rand() % 256;
-            }
-            io_request req = {fd, offset, length, buffer, false};
-            submit_io(&ring, &req);
-            wait_completion(&ring);
-        }
-    }
+	ret = io_uring_submit(&ring);
+	if (ret < 0) {
+		fprintf(stderr, "io_uring_submit: %s\n", strerror(-ret));
+		return 1;
+	} else if (ret != 1) {
+		fprintf(stderr, "io_uring_submit submitted less %d\n", ret);
+		return 1;
+	}
 
-    // Cleanup
-    delete[] buffer;
-    close(fd);
-    io_uring_queue_exit(&ring);
+	done = 0;
+	pending = ret;
+	fsize = 0;
+	for (i = 0; i < 1; i++) {
+		ret = io_uring_wait_cqe(&ring, &cqe);
+		//printf("%d\n",ret);
+		printf("%d\n",cqe->res);
+		//if (ret < 0) {
+		//	fprintf(stderr, "io_uring_wait_cqe: %s\n", strerror(-ret));
+		//	return 1;
+		//}
 
-    return 0;
+		done++;
+		ret = 0;
+		if (cqe->res != 4096 && cqe->res + fsize != sb.st_size) {
+			fprintf(stderr, "ret=%d, wanted 4096\n", cqe->res);
+			ret = 1;
+		}
+		fsize += cqe->res;
+		io_uring_cqe_seen(&ring, cqe);
+	//	if (ret)
+	//		break;
+	}
+
+	printf("Submitted=%d, completed=%d, bytes=%lu\n", pending, done,
+						(unsigned long) fsize);
+	printf("cqe->usr_data:%llu\n",cqe->user_data);
+	close(fd);
+	io_uring_queue_exit(&ring);
+	return 0;
 }
